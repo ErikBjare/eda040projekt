adc757d9e32b3eeb41cb4cf4510d21cebd1f43b2
essenji <essenjisimon@gmail.com>
2015-11-08 16:06:56 +0100

    added system

---
+++ src/client/System.java
@@ -0,0 +1,33 @@
+package client;
+
+import client.camera.Camera;
+
+import java.util.ArrayList;
+
+/**
+ * Created by simon on 2015-11-08.
+ */
+public class System {
+    private ArrayList<Camera> cameraList;
+    private byte[][] currentFrames;
+    public System(){
+
+
+    }   
+
+    public synchronized void registerDelay(long captureTime){
+
+
+    }
+
+    public synchronized void motionDetected(){
+
+    }
+
+
+
+
+
+
+
+}
