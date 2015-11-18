89c908070e31de184af5d4c5cbb06dd8924e5661
Kommendant <jonasd90@gmail.com>
2015-11-18 19:08:55 +0100

    Moved logic from Sender to Monitor

--- src/server/Monitor.java
+++ src/server/Monitor.java
@@ -1,18 +1,31 @@
 package server;
 
 import client.Mode;
+import common.protocol.Constants;
+import common.protocol.Message;
+import common.protocol.NewFrame;
 import se.lth.cs.eda040.fakecamera.AxisM3006V;
+
+import java.io.IOException;
+import java.net.Socket;
 
 /**
  * Created by von on 2015-11-05.
  */
 public class Monitor {
+    private final Socket sendSocket;
     AxisM3006V camera;
     byte[] lastFrame;
     boolean motionDetected;
     long timeStamp;
     Mode mode;
+    private long lastSentFrameTime;
 
+    public Monitor(Socket sendSocket){
+
+
+        this.sendSocket = sendSocket;
+    }
 
     public synchronized void newFrame(long time, boolean motion, byte[] frame){
 
@@ -28,9 +41,37 @@ public class Monitor {
         camera.destroy();
     }
     public synchronized void sendNext(){
+        try {
+            getReadyToSend();
+            Message mess = new NewFrame(lastFrame.length, lastFrame, timeStamp);
+            mess.send(sendSocket);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        lastSentFrameTime = System.currentTimeMillis();
+
 
     }
     public synchronized void setMode(Mode newMode){
+
+    }
+    private void getReadyToSend() throws InterruptedException {
+        long frameMsInterval;
+        if (mode == Mode.Idle || mode == Mode.ForceIdle) {
+            frameMsInterval = 1000 / Constants.IDLE_FRAMERATE;
+        } else {
+            frameMsInterval = 1000 / Constants.MOVIE_FRAMERATE;
+        }
+        long timeDiff = System.currentTimeMillis() - lastSentFrameTime;
+
+        if (timeDiff < frameMsInterval) {
+            wait(frameMsInterval - timeDiff);
+        }
 
     }
 }
--- src/server/Sender.java
+++ src/server/Sender.java
@@ -11,38 +11,18 @@ import java.net.Socket;
 public class Sender extends Thread {
     private Monitor monitor;
     private Socket socket;
-    private long lastSentFrameTime;
 
 
-    public Sender(Monitor monitor, Socket socket) {
+
+    public Sender(Monitor monitor) {
         this.monitor = monitor;
-        this.socket = socket;
+
     }
 
     public void run() {
 
-        try {
-            getReadyToSend(monitor.mode);
-            monitor.sendNext();
-            lastSentFrameTime = System.currentTimeMillis();
-            
-            } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+                        monitor.sendNext();
+                      }
 
-    private void getReadyToSend(Mode mode) throws InterruptedException {
-        long frameMsInterval;
-        if (mode == Mode.Idle || mode == Mode.ForceIdle) {
-            frameMsInterval = 1000 / Constants.IDLE_FRAMERATE;
-        } else {
-            frameMsInterval = 1000 / Constants.MOVIE_FRAMERATE;
-        }
-        long timeDiff = System.currentTimeMillis() - lastSentFrameTime;
 
-        if (timeDiff < frameMsInterval) {
-            wait(frameMsInterval - timeDiff);
-        }
-
-    }
 }
