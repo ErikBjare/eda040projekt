fda990dbe16c49a66c09c0adfb30300960cb477c
Kommendant <jonasd90@gmail.com>
2015-11-18 18:21:15 +0100

    Implemented sender thread and added constants

--- src/server/Sender.java
+++ src/server/Sender.java
@@ -1,5 +1,8 @@
 package server;
 
+import client.Mode;
+import common.protocol.Constants;
+
 import java.net.Socket;
 
 /**
@@ -8,13 +11,38 @@ import java.net.Socket;
 public class Sender extends Thread {
     private Monitor monitor;
     private Socket socket;
+    private long lastSentFrameTime;
+
 
     public Sender(Monitor monitor, Socket socket) {
         this.monitor = monitor;
         this.socket = socket;
     }
 
-    public void run(){
+    public void run() {
+
+        try {
+            getReadyToSend(monitor.mode);
+            monitor.sendNext();
+            lastSentFrameTime = System.currentTimeMillis();
+            
+            } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getReadyToSend(Mode mode) throws InterruptedException {
+        long frameMsInterval;
+        if (mode == Mode.Idle || mode == Mode.ForceIdle) {
+            frameMsInterval = 1000 / Constants.IDLE_FRAMERATE;
+        } else {
+            frameMsInterval = 1000 / Constants.MOVIE_FRAMERATE;
+        }
+        long timeDiff = System.currentTimeMillis() - lastSentFrameTime;
+
+        if (timeDiff < frameMsInterval) {
+            wait(frameMsInterval - timeDiff);
+        }
 
     }
 }
