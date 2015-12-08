718102961ffacbc60a1a8d46d30a57584a164b7e
Valthor <vlthrh@gmail.com>
2015-12-08 15:49:23 +0100

    added configuration for jar file artifacts

--- src/ProxyGUIMain.java
+++ src/ProxyGUIMain.java
@@ -8,14 +8,14 @@ import java.util.stream.IntStream;
 /**
  * Created by Tank on 12/2/2015.
  */
-public class ProxyMain {
+public class ProxyGUIMain {
 
     /**
      * Starts both the GUI and proxy servers
      * @param args The list of argus ids to connect to (
      *             Giving 0 arguments defaults to argus 1-4.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         if (args.length == 0) args = new String[]{"1", "2", "3", "4"};
         final String[] finalArgs = args;
         for (int i = 0; i < args.length; i++) {
@@ -23,7 +23,7 @@ public class ProxyMain {
             Thread t = new Thread(()->server.Main.start(Constants.PROXY_PORT_START + finalI, new ProxyAxis("argus-"+ finalArgs[finalI]+".student.lth.se", Constants.CAMERA_PORT)));
             t.start();
         }
-
+        Thread.sleep(1000);
         String[] hosts = IntStream.iterate(Constants.PROXY_PORT_START, x->x+1)
                 .limit(args.length)
                 .mapToObj(i -> "localhost " + i)
